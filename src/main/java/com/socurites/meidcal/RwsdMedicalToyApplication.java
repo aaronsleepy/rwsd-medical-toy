38bb9828e3141ab009e681cc18ab534a4e224ae9
aaronsleepy <aaron@kmong.com>
2022-05-01 21:22:22 +0900

    first commit

---
+++ src/main/java/com/socurites/meidcal/RwsdMedicalToyApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.meidcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsdMedicalToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwsdMedicalToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socurites/meidcal/RwsdMedicalToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socurites.meidcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwsdMedicalToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
