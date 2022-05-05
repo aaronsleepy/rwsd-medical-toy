065f0312fabb404d75f1394c496dbc8bb36a9d74
aaronsleepy <aaron@kmong.com>
2022-05-05 21:17:45 +0900

    feature: Add InvoiceImporter 클래스 - TextFile 구현 추가

--- src/main/java/com/socurites/meidcal/domain/TextFile.java
+++ src/main/java/com/socurites/meidcal/domain/TextFile.java
@@ -1,16 +1,32 @@
 package com.socurites.meidcal.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-@AllArgsConstructor
 @Builder
 @Getter
 public class TextFile {
     private final Map<String, String> attributes;
     private final List<String> lines;
+
+    public TextFile(final File file) throws IOException {
+        this.attributes = new HashMap<>();
+        this.attributes.put(Attributes.PATH, file.getPath());
+        this.lines = Files.lines(file.toPath()).collect(Collectors.toList());
+    }
+
+    public void addLineSuffix(final String prefix, final String attributeName) {
+        lines.stream()
+                .filter(l -> l.startsWith(prefix))
+                .findAny()
+                .ifPresent(l -> attributes.put(attributeName, l.substring(prefix.length())));
+    }
 }
---
+++ src/main/java/com/socurites/meidcal/importer/InvoiceImporter.java
@@ -0,0 +1,26 @@
+package com.socurites.meidcal.importer;
+
+import com.socurites.meidcal.domain.Attributes;
+import com.socurites.meidcal.domain.Document;
+import com.socurites.meidcal.domain.TextFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class InvoiceImporter implements Importer {
+    private static final String NAME_PREFIX = "Dear ";
+    private static final String AMOUNT_PREFIX = "Amount: ";
+
+    @Override
+    public Document importFile(final File file) throws IOException {
+        final TextFile textFile = new TextFile(file);
+        textFile.addLineSuffix(NAME_PREFIX, Attributes.PATIENT);
+        textFile.addLineSuffix(AMOUNT_PREFIX, Attributes.AMOUNT);
+
+        final Map<String, String> attributes = textFile.getAttributes();
+        attributes.put(Attributes.TYPE, "INVOICE");
+
+        return new Document(attributes);
+    }
+}
