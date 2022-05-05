f8d70f7b5aecc0e690ce4385783d2ba36e423372
aaronsleepy <aaron@kmong.com>
2022-05-05 18:18:09 +0900

    feature: importFile 구현

---
+++ src/main/java/com/socurites/meidcal/exception/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package com.socurites.meidcal.exception;
+
+public class UnknownFileTypeException extends RuntimeException {
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
--- src/main/java/com/socurites/meidcal/main/DocumentManagementSystem.java
+++ src/main/java/com/socurites/meidcal/main/DocumentManagementSystem.java
@@ -1,15 +1,48 @@
 package com.socurites.meidcal.main;
 
+import com.socurites.meidcal.domain.Document;
+import com.socurites.meidcal.exception.UnknownFileTypeException;
 import com.socurites.meidcal.importer.ImageImporter;
 import com.socurites.meidcal.importer.Importer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
+    private final List<Document> documents = new ArrayList<>();
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
         extensionToImporter.put("letter", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileSystemNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found for file: " + path);
+            }
+
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (null != importer) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        }
+
     }
 }
